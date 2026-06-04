dfdba2a75c6644d84a88c63fa052444b43a6b9cb
Minahil Ali <manahilali117@gmail.com>
2026-06-04 17:03:49 +0500

    changes

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,14 +1,16 @@
 import { createClient } from '@/lib/supabase/server'
 import { redirect } from 'next/navigation'
+import { headers } from 'next/headers'
 
 export default async function LoginPage() {
   async function signInWithGoogle() {
     'use server'
+    const origin = (await headers()).get('origin')
     const supabase = await createClient()
-    const { data, error } = await supabase.auth.signInWithOAuth({
+    const { data } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
+        redirectTo: `${origin}/auth/callback`,
       },
     })
 
